a7ebf923f153d70c08a76475975bd5ff921161a4
Ebrahimgh12 <nosrati78ebrahim1@gmail.com>
2024-10-28 05:20:47 +0330

    bot.py

---
+++ bot.py
@@ -0,0 +1,23 @@
+from telebot import TeleBot
+from telebot.types import (Message, InlineKeyboardMarkup,
+                           InlineKeyboardButton, WebAppInfo)
+
+
+bot = TeleBot('7684900800:AAH-YIxTng3cb-BgYTbqFIE0HBqpX5MOdto')
+
+
+@bot.message_handler(commands=['start'])
+def start(message: Message):
+    markup = InlineKeyboardMarkup([
+        [InlineKeyboardButton(text='Your MiniApp',
+                              web_app=WebAppInfo(
+                                  ''
+                              ))]
+    ])
+
+    bot.send_message(chat_id=message.chat.id, text='بفرمایید وب اپ شما:', reply_markup=markup)
+
+
+if __name__ == '__main__':
+    bot.polling(skip_pending=True)
+  
